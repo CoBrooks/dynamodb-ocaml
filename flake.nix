fc9061938a29c96146f9ce10517d89678aaa3e97
Cole Brooks <cole.brooks@colebrooks.com>
2023-08-25 12:00:38 -0500

    Add musl-compiled package to flake output

--- flake.nix
+++ flake.nix
@@ -10,28 +10,55 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in rec {
-    packages.${system}.default = pkgs.ocamlPackages.buildDunePackage {
-      pname = "dynamodb";
-      version = "0.1.0";
-      src = ./.;
+    packages.${system} = {
+      default = pkgs.ocamlPackages.buildDunePackage {
+        pname = "dynamodb";
+        version = "0.1.0";
+        src = ./.;
 
-      duneVersion = "3";
+        duneVersion = "3";
 
-      propagatedBuildInputs = with pkgs.ocamlPackages; [
-        cohttp
-        cohttp-lwt-unix
-        digestif
-        lwt
-        ppx_deriving
-        ppx_deriving_yojson
-        uri
-        yojson
-      ];
+        propagatedBuildInputs = with pkgs.ocamlPackages; [
+          cohttp
+          cohttp-lwt-unix
+          digestif
+          lwt
+          ppx_deriving
+          ppx_deriving_yojson
+          uri
+          yojson
+        ];
 
-      nativeBuildInputs = with pkgs; [
-        ocaml
-        ocamlPackages.findlib
-      ];
+        nativeBuildInputs = with pkgs; [
+          ocaml
+          ocamlPackages.findlib
+        ];
+      };
+      musl = let
+        inherit (pkgs) pkgsMusl;
+      in pkgsMusl.ocamlPackages.buildDunePackage {
+        pname = "dynamodb";
+        version = "0.1.0";
+        src = ./.;
+
+        duneVersion = "3";
+
+        propagatedBuildInputs = with pkgsMusl.ocamlPackages; [
+          cohttp
+          cohttp-lwt-unix
+          digestif
+          lwt
+          ppx_deriving
+          ppx_deriving_yojson
+          uri
+          yojson
+        ];
+
+        nativeBuildInputs = with pkgsMusl; [
+          ocaml
+          ocamlPackages.findlib
+        ];
+      };
     };
 
     devShells.${system}.default = pkgs.mkShell {
